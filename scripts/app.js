80283fdcbabd29b615777b591a02b93a16a8fb0f
Franco Estevez <francoestevez2008@gmail.com>
2021-04-04 19:06:36 -0300

    1.2 animating the canvas

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,41 @@
 const myfunction = () => {
     alert ("hola")
 }
+
+
 let canvas = null;
 let context = null;
+let x = 50;
+let y = 50;
+
 
 const paint = (context) => {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
     context.fillStyle = '#0f0';
-    context.fillRect(50, 50, 100, 60);
+    context.fillRect(x, y, 10, 10);
+}
+
+const act = () => {
+    x += 5;
+    if (x > canvas.width) {
+        x = 0;
+        }
+}
+
+const run = () => {
+    window.requestAnimationFrame(run);
+    act();
+    paint(context);
 }
 
 const init = () => {
     canvas = document.getElementById('canvas')
     context = canvas.getContext('2d');
-    paint(context);
+    run();
 }
+
 window.addEventListener('load', init, false);
+
+
 
